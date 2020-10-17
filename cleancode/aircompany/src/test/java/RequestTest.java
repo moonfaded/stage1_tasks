3bea07f4a905c918a448be70da0feea5af7a751a
Alexander Vladyko <vladyko.alexander@gmail.com>
2020-10-17 10:51:07 +0300

    Unit test fixes & new tests created

--- cleancode/aircompany/src/test/java/RequestTest.java
+++ cleancode/aircompany/src/test/java/RequestTest.java
@@ -9,29 +9,56 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RequestTest {
-  private static final List<Plane> planes = Arrays.asList(
+  protected static final List<Plane> planes = Arrays.asList(
       new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
       new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
-      new PassengerPlane("Boeing-747", 980, 16100, 70500, 242),
-      new PassengerPlane("Airbus A320", 930, 11800, 65500, 188),
-      new PassengerPlane("Airbus A330", 990, 14800, 80500, 222),
-      new PassengerPlane("Embraer 190", 870, 8100, 30800, 64),
-      new PassengerPlane("Sukhoi Superjet 100", 870, 11500, 50500, 140),
-      new PassengerPlane("Bombardier CS300", 920, 11000, 60700, 196),
       new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryPlaneType.BOMBER),
       new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryPlaneType.BOMBER),
-      new MilitaryPlane("B-52 Stratofortress", 1000, 20000, 80000, MilitaryPlaneType.BOMBER),
-      new MilitaryPlane("F-15", 1500, 12000, 10000, MilitaryPlaneType.FIGHTER),
-      new MilitaryPlane("F-22", 1550, 13000, 11000, MilitaryPlaneType.FIGHTER),
       new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryPlaneType.TRANSPORT)
   );
 
+  protected static final List<Plane> planesSortedByMaxDistance = Arrays.asList(
+      new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryPlaneType.TRANSPORT),
+      new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
+      new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
+      new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryPlaneType.BOMBER),
+      new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryPlaneType.BOMBER)
+  );
+
+  protected static final List<Plane> planesSortedByMaxSpeed = Arrays.asList(
+      new MilitaryPlane("C-130 Hercules", 650, 5000, 110000, MilitaryPlaneType.TRANSPORT),
+      new PassengerPlane("Boeing-737", 900, 12000, 60500, 164),
+      new PassengerPlane("Boeing-737-800", 940, 12300, 63870, 192),
+      new MilitaryPlane("B-2 Spirit", 1030, 22000, 70000, MilitaryPlaneType.BOMBER),
+      new MilitaryPlane("B-1B Lancer", 1050, 21000, 80000, MilitaryPlaneType.BOMBER)
+  );
+
+  Request request = new Request(planes);
+
   @Test
-  public void testGetPassengerPlaneWithMaxCapacity() {
-    System.out.println("TEST testGetPassengerPlaneWithMaxCapacity started!");
-    Request request = new Request(planes);
-    Plane planeWithMaxPassengerCapacity = planes.get(2);
-    PassengerPlane expectedPlaneWithMaxPassengersCapacity = request.getPassengerPlaneWithMaxPassengersCapacity();
-    Assert.assertEquals(planeWithMaxPassengerCapacity, expectedPlaneWithMaxPassengersCapacity);
+  public void getPassengerPlanesTest() {
+    Assert.assertEquals(request.getPassengerPlanes().size(), 2);
+  }
+
+  @Test
+  public void getMilitaryPlanesTest() {
+    Assert.assertEquals(request.getMilitaryPlanes().size(), 3);
+  }
+
+  @Test
+  public void getPassengerPlaneWithMaxPassengersCapacityTest() {
+    Assert.assertEquals(request.getPassengerPlaneWithMaxPassengersCapacity(), planes.get(1));
+  }
+
+  @Test
+  public void sortByMaxDistanceTest() {
+    request.sortByMaxDistance();
+    Assert.assertEquals(planes, planesSortedByMaxDistance);
+  }
+
+  @Test
+  public void sortByMaxSpeedTest() {
+    request.sortByMaxSpeed();
+    Assert.assertEquals(planes, planesSortedByMaxSpeed);
   }
 }
